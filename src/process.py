9ee21b106b9a4d8e704be693c6ce04255fab4e52
Dmitriy <dikorpshy@mail.ru>
2024-03-25 16:41:37 +0300

    pickle

--- src/process.py
+++ src/process.py
@@ -1,20 +1,20 @@
-"""
-This is the demo code that uses hydra to access the parameters in under the directory config.
+# """
+# This is the demo code that uses hydra to access the parameters in under the directory config.
 
-Author: Khuyen Tran
-"""
+# Author: Khuyen Tran
+# """
 
-import hydra
-from omegaconf import DictConfig
-
-
-@hydra.main(config_path="../config", config_name="main", version_base="1.2")
-def process_data(config: DictConfig):
-    """Function to process the data"""
-
-    print(f"Process data using {config.data.raw}")
-    print(f"Columns used: {config.process.use_columns}")
+# import hydra
+# from omegaconf import DictConfig
 
 
-if __name__ == "__main__":
-    process_data()
+# @hydra.main(config_path="../config", config_name="main", version_base="1.2")
+# def process_data(config: DictConfig):
+#     """Function to process the data"""
+
+#     print(f"Process data using {config.data.raw}")
+#     print(f"Columns used: {config.process.use_columns}")
+
+
+# if __name__ == "__main__":
+#     process_data()
